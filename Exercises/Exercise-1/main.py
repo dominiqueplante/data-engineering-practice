fa01e910acf156f152f6b76ff75fc0d4e03fbe24
Dominique Plante <dominique.plante@gmail.com>
2025-05-16 16:21:09 -0700

    Exercise 1

--- Exercises/Exercise-1/main.py
+++ Exercises/Exercise-1/main.py
@@ -1,4 +1,6 @@
 import requests
+import os
+import zipfile
 
 download_uris = [
     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2018_Q4.zip",
@@ -10,11 +12,35 @@ download_uris = [
     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2220_Q1.zip",
 ]
 
+download_dir = "downloads"
+
+
+def processFile(downlaod_dir, uri):
+    uri_split = uri.split("/")
+    filename = uri_split[-1]
+    response = requests.get(uri)
+    dest_file = f"{download_dir}/{filename}"
+    if response.status_code == 200:
+        with open(dest_file, "wb") as file:
+            file.write(response.content)
+        print(f"Successfully downloaded the file: {dest_file}")
+        print(f"Unzipping: {dest_file}")
+        # Each file is a `zip`, extract the `csv` from the `zip` and delete the `zip` file.
+        with zipfile.ZipFile(dest_file, 'r') as zObject:
+            zObject.extractall(path=f"{download_dir}")
+        print(f"Removing zip file: {dest_file}")
+        os.remove(f"{dest_file}")
+    else:
+        print(f"Failed to download file {dest_file}. Status code: {response.status_code}")
 
 def main():
     # your code here
-    pass
-
+    # create the directory `downloads` if it doesn't exist
+    os.makedirs(download_dir, exist_ok=True)
+    # download the files one by one.
+    for uri in download_uris:
+        processFile(download_dir, uri)
+    print("Done!")
 
 if __name__ == "__main__":
     main()
